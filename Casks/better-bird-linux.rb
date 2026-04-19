821a1ad2a26c1654680a3af79ca67fe147edd9c2
PRATYAY MUSTAFI <pratyaymustafi@outlook.com>
2026-04-19 12:31:42 +0530

    init better bird

---
+++ Casks/better-bird-linux.rb
@@ -0,0 +1,45 @@
+# Documentation: https://docs.brew.sh/Cask-Cookbook
+#                https://docs.brew.sh/Adding-Software-to-Homebrew#cask-stanzas
+# PLEASE REMOVE ALL GENERATED COMMENTS BEFORE SUBMITTING YOUR PULL REQUEST!
+cask "better-bird-linux" do
+  version "140.9.0esr-bb20"
+  sha256 "62d040cb5bf3e7d35ece4d26700f9324f3e17367648421e5e177c7d19a393024"
+
+  url "https://www.betterbird.eu/downloads/LinuxArchive/betterbird-#{version}.en-US.linux-x86_64.tar.xz"
+  name "better-bird-linux"
+  desc "mail client firefox"
+  homepage "https://www.betterbird.eu/"
+
+  livecheck do
+    url "https://www.betterbird.eu/downloads/getloc.php?os=linux&lang=en-US&version=release/"
+    strategy :page_match
+    regex(/href=.*?betterbird[._-]v?(\d+(?:\.\d+)+)\.en-US\.linux-x86_64\.tar\.xz/i)
+  end
+
+  binary "betterbird/betterbird"
+  artifact "betterbird/betterbird.desktop",
+           target: "#{Dir.home}/.local/share/applications/betterbird.desktop"
+  artifact "betterbird/chrome/icons/default/default128.png",
+           target: "#{Dir.home}/.local/share/icons/betterbird.png"
+
+  preflight do
+    FileUtils.mkdir_p "#{Dir.home}/.local/share/applications"
+    File.write("#{staged_path}/betterbird/betterbird.desktop", <<~EOS)
+      [Desktop Entry]
+      Name=BetterBird
+      Keywords=internet,email,mail,web
+      Exec=#{HOMEBREW_PREFIX}/bin/betterbird %u
+      Icon=#{Dir.home}/.local/share/icons/betterbird.png
+      Terminal=false
+      Type=Application
+      StartupWMClass=BetterBird
+      Categories=Api;Code;Development;Text;Edit;Editor;
+      Actions=new-empty-window;new-private-window;
+
+    EOS
+  end
+  zap trash: [
+    "~/.config/firefox",
+    "~/.mozilla",
+  ]
+end
